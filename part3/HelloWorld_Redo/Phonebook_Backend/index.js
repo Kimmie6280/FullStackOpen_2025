157f929cb35be69ae455e35033640341aff8d8e6
Kimberly Ramgopal <66437777+Kimmie6280@users.noreply.github.com>
2025-06-30 17:36:47 -0400

    Update index.js

--- part3/HelloWorld_Redo/Phonebook_Backend/index.js
+++ part3/HelloWorld_Redo/Phonebook_Backend/index.js
@@ -1,9 +1,9 @@
-
 const express = require('express')
 const app = express()
 app.use(express.json())
 
-let persons=[
+let persons =
+[
     { 
       "id": "1",
       "name": "Arto Hellas", 
@@ -23,14 +23,25 @@ let persons=[
       "id": "4",
       "name": "Mary Poppendieck", 
       "number": "39-23-6423122"
+    },
+    {
+      "id": "5",
+      "name": "Kimberly Ramgopal", 
+      "number": "347-239-2516"
     }
 ]
 
-app.get('/',(request,response) =>{
-     response.send('<h1>Hello World!</h1>')
+app.get('/', (request, response) => {
+  response.send('<h1>good bye World!</h1>')
 })
 
-//getting request time
+//3.1
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+//3.2
 const requestTime = function (req, res, next) {
   req.requestTime = Date.now(); // Stores the timestamp in milliseconds
   next(); // Passes control to the next middleware or route handler
@@ -38,7 +49,7 @@ const requestTime = function (req, res, next) {
 
 app.use(requestTime); // Apply the middleware globally
 
-app.get('/api/info',(request,response) =>{
+app.get('/info',(request,response) =>{
     const pLength = persons.length
     const dateString = new Date(request.requestTime).toLocaleString('en-US', {
     timeZone: 'America/New_York',
@@ -49,22 +60,65 @@ app.get('/api/info',(request,response) =>{
   );
 })
 
+//3.3
 
-app.get('/api/persons',(request,response) => {
-    response.json(persons)
+app.get('/api/persons/:id', (request,response) =>{
+  const id = request.params.id
+  const person = persons.find(person => person.id === id)
+
+  if (person){
+    response.json(person)
+  }else{
+    response.status(404).end()
+  }
 })
 
-app.get('/api/persons/:id', (request, response) =>{
-    const id =  request.params.id
-    const person = persons.find(person => person.id === id)
+//3.4
 
-    if (person){
-        response.json(person)
+app.delete('/api/persons/:id', (request,response) =>{
+  const id = request.params.id
+  const person = persons.filter(person => person.id !== id)
+  response.status(204).end()
+})
+
+//3.5 3.6
+const generateId = () => {
+   
+    const newId = Math.floor(Math.random() * 10000)
+    return newId
+}
+
+app.post('/api/persons',(request,response) =>{
+    const body = request.body
+
+     if (!body.name) {
+        return response.status(400).json({ 
+        error: 'Name missing' 
+      });
     }
-    else{
-        response.status(404).end()
-    }
+
+      if (!body.number) {
+        return response.status(400).json({ 
+          error: 'Number missing' 
+        });
+      }
+
+      if (persons.find(p => p.name === body.name)) {
+        return response.status(400).json({ error: 'name must be unique' });
+      }
+
+   const person = {
+    name: body.name,    
+    number: body.number,
+    id: generateId(),
+  }; 
   
+  console.log(person)
+
+
+    persons = persons.concat(person)
+    response.json(person)
+
 
 })
 
